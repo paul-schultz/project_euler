655bc9baae6b4acd267c1388e48b5d8b8b82a03c
paul-schultz <paul.william.schultz@gmail.com>
2020-10-12 17:03:15 -0700

    md write up for problem 03

--- problems/03/primeFactor.js
+++ problems/03/primeFactor.js
@@ -1,24 +1,24 @@
 function largestPrimeFactor(num) {
-    var arr = [];
-    var isPrime;
-    for (var i = 2, sq = Math.sqrt(num); i <= sq ; i++) {
-      if (num % i === 0) {
-        for(var j = 2; j <= i/2; j++) {
-          if ( i % j == 0) {
-            isPrime = false;
-          } else {
-            isPrime = true;
-          }
-        }
-        if(isPrime = true) {
-          num /= i;
-          arr.push(i)
+  var arr = [];
+  var isPrime;
+  for (var i = 2, sq = Math.sqrt(num); i <= sq ; i++) {
+    if (num % i === 0) {
+      for(var j = 2; j <= i/2; j++) {
+        if ( i % j == 0) {
+          isPrime = false;
+        } else {
+          isPrime = true;
         }
       }
+      if(isPrime = true) {
+        num /= i;
+        arr.push(i)
+      }
     }
-    var solution = Math.max(...arr);
-    return solution;
-  };
+  }
+  var solution = Math.max(...arr);
+  return solution;
+};
   
   
   module.exports = largestPrimeFactor;
