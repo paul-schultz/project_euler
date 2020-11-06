c171c5cba844a7ca58bd38f06139ca2890215812
paul-schultz <paul.william.schultz@gmail.com>
2020-11-06 10:20:45 -0800

    problem 15 final code, passing tests

--- problems/15/latticePaths.js
+++ problems/15/latticePaths.js
@@ -1,3 +1,24 @@
-function latticePaths() {} 
+function latticePaths(a,b) {
+    var n = a + b,
+        k = a,
+        z = n - k;
+    
+    var nB = n,
+        kB = k,
+        zB = z;
+    
+    for (var i = n-1; i >= 1; i--) {
+        nB *= i;
+    }
+    for (var i = k-1; i >= 1; i--) {
+        kB *= i;
+    }
+    for (var i = z-1; i >= 1; i--) {
+        zB *= i;
+    }
+    
+    var paths = Math.floor(nB / (kB * zB));
+    return paths 
+}
  
 module.exports = latticePaths    
--- problems/15/latticePaths.test.js
+++ problems/15/latticePaths.test.js
@@ -1,5 +1,17 @@
 const latticePaths = require('./latticePaths') 
 
-test('test caption', () => { 
-    expect(latticePaths(num)).toBe(num); 
+test('A 2x2 grid has 6 routes', () => { 
+    expect(latticePaths(2,2)).toBe(6); 
 });     
+
+test('A 5x10 grid has 3003 routes', () => { 
+    expect(latticePaths(5,10)).toBe(3003); 
+});     
+
+test('A 12x10 grid has 646646 routes', () => { 
+    expect(latticePaths(12,10)).toBe(646646); 
+});  
+
+test('A 20x20 grid has 137846528820 routes', () => { 
+    expect(latticePaths(20,20)).toBe(137846528820); 
+});  
