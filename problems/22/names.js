eea426660c4ac48d1aee594e85762b0d8ee2a557
paul-schultz <paul.william.schultz@gmail.com>
2020-11-13 09:03:57 -0800

    problem 22 working solution, final code

--- problems/22/names.js
+++ problems/22/names.js
@@ -1,3 +1,37 @@
-function names() {} 
+var fs = require('fs');
+
+const score = [
+    "A", "B", "C", "D", 
+    "E", "F", "G", "H", 
+    "I", "J", "K", "L", 
+    "M", "N", "O", "P", 
+    "Q", "R", "S", "T", 
+    "U", "V", "W", "X", 
+    "Y", "Z"
+]
+
+var list = fs.readFileSync('names.txt').toString().split(',').sort()
+
+function names() {
+    var totalSum = 0;
+
+    for (var i = 0; i <= list.length - 1; i++) {
+        var name = list[i].replace(/['"]+/g, '')
+        var split = name.split('')
+
+        var sum = 0
+        for (var j = 0; j < split.length ; j++) {
+            sum += score.indexOf(split[j]) + 1
+        }
+
+        var sumIndex = sum * (i + 1)
+        totalSum += sumIndex
+    }
+    
+    return totalSum
+}
+
+
+
  
 module.exports = names    
--- problems/22/worksheet.js
+++ problems/22/worksheet.js
@@ -1,9 +1,38 @@
 var fs = require('fs');
 
+const score = [
+    "A", "B", "C", "D", 
+    "E", "F", "G", "H", 
+    "I", "J", "K", "L", 
+    "M", "N", "O", "P", 
+    "Q", "R", "S", "T", 
+    "U", "V", "W", "X", 
+    "Y", "Z"
+]
+
+
 var list = fs.readFileSync('names.txt').toString().split(',').sort()
 
 function names() {
-    console.log(list[937].replace(/['"]+/g, ''))
+    var totalSum = 0;
+
+    for (var i = 0; i <= list.length - 1; i++) {
+        var name = list[i].replace(/['"]+/g, '')
+        var split = name.split('')
+        // console.log(name)
+
+        var sum = 0
+        for (var j = 0; j < split.length ; j++) {
+            sum += score.indexOf(split[j]) + 1
+            // console.log(sum)
+        }
+        // console.log(sum)
+        var sumIndex = sum * (i + 1)
+
+        totalSum += sumIndex
+    }
+    
+    console.log(totalSum)
 }
 
-names()
+names() //returns 871198282 which is correct
