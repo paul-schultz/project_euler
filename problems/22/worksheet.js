1a359987b5243d68190102f09faf2fb4f5edb011
paul-schultz <paul.william.schultz@gmail.com>
2020-11-13 08:24:20 -0800

    list file added to problem 22

--- problems/22/worksheet.js
+++ problems/22/worksheet.js
@@ -1,1 +1,9 @@
-function names() {}
+var fs = require('fs');
+
+var list = fs.readFileSync('names.txt').toString().split(',').sort()
+
+function names() {
+    console.log(list[937].replace(/['"]+/g, ''))
+}
+
+names()
